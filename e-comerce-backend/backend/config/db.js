83642935c0ce49fbbbf80599043696bc13159f13
Sangam Parmar <118618529+sangamparmar@users.noreply.github.com>
2024-07-20 11:42:56 +0530

    Update db.js

--- e-comerce-backend/backend/config/db.js
+++ e-comerce-backend/backend/config/db.js
@@ -3,19 +3,16 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb+srv://itsinstagram69:sangam27@cluster0.whcleb7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    )
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
     console.log('MongoDB connection SUCCESS')
   } catch (error) {
-    console.error('MongoDB connection FAIL')
+    console.error('MongoDB connection FAIL:', error.message)
     process.exit(1)
   }
 }
 
-module.exports = {connectDB}
+module.exports = { connectDB }
